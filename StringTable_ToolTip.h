6092aee9256f8c1b0bdf1b53ebd0755d21ea3f2c
JiaRong <s451278963@gmail.com>
2016-03-04 17:13:37 +0800

    快鍵提示整合

---
+++ StringTable_ToolTip.h
@@ -0,0 +1,33 @@
+#pragma once
+/*工具提示字串表*/
+
+//MAIN Page
+
+#define						TT_BTNMAINSTART					_T("加工開始")
+#define						TT_BTNMAINGOORIGIN			    _T("回歸初始點")
+#define						TT_BTNMAINTIMEOUT				_T("做完一次動作暫停")
+#define						TT_BTNMAINSTOP					_T("立即停止")
+
+//COMPILER Page
+
+//PARAMETER Page
+
+#define						TT_BTNPARSETFINISH				_T("點擊後完成儲存")
+#define						TT_BTNPAREDITPAR				_T("編輯所有參數")
+#define						TT_BTNPARINIT					_T("點擊後初始所有設定")
+#define						TT_CHKPARHLIMIT					_T("請勿使用")
+#define						TT_CHKPARSLIMIT					_T("小心使用")
+
+//IOSTATE Page
+
+#define						TT_BTNIOREFRESH					_T("刷新IO數值")
+#define						TT_BTNIOOPENREFRESH				_T("開啟或關閉刷新IO數值")
+#define						TT_BTNIOCOMPILER				_T("編輯所有資料")
+#define						TT_BTNSETFINISH					_T("完成設定")
+
+//TOOLBAR
+
+#define						TT_BTNIACCOUNT					_T("登出")
+#define						TT_BTNINPROCESS					_T("載入料件")
+#define						TT_BTNOBJMENAGEMENT			    _T("料件管理")
+#define						TT_BTNHISTORY					_T("歷史訊息")
